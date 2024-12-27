d2b9e81ec685c0ae9b528e881dc997d7c45ccfe6
Cekepek <ckpk7171@gmail.com>
2024-12-27 17:05:37 +0700

    input email

--- lib/screen/emailLupaPassword.dart
+++ lib/screen/emailLupaPassword.dart
@@ -1,282 +1,145 @@
-// import 'dart:convert';
+import 'dart:convert';
 
-// import 'package:flutter/material.dart';
-// import 'package:lectio_divina/model/api.dart' as api;
-// import 'package:lectio_divina/globals.dart' as globals;
-// import 'package:lectio_divina/screen/login.dart';
+import 'package:flutter/material.dart';
+import 'package:lectio_divina/model/api.dart' as api;
+import 'package:lectio_divina/globals.dart' as globals;
+import 'package:lectio_divina/screen/login.dart';
 
-// // // class Emaillupapassword extends StatelessWidget {
-// // //   const Emaillupapassword({super.key});
+// // class Emaillupapassword extends StatelessWidget {
+// //   const Emaillupapassword({super.key});
 
-// // //   @override
-// // //   Widget build(BuildContext context) {
-// // //     return MaterialApp(
-// // //       title: 'LECTIO DIVINA',
-// // //       theme: ThemeData(
-// // //         fontFamily: "Poppins",
-// // //         primaryColor: globals.colorTheme,
-// // //         colorScheme: ColorScheme.fromSeed(seedColor: globals.colorTheme),
-// // //         useMaterial3: true,
-// // //       ),
-// // //       home: const EmailLupaPasswordState(),
-// // //     );
-// // //   }
-// // // }
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       title: 'LECTIO DIVINA',
+// //       theme: ThemeData(
+// //         fontFamily: "Poppins",
+// //         primaryColor: globals.colorTheme,
+// //         colorScheme: ColorScheme.fromSeed(seedColor: globals.colorTheme),
+// //         useMaterial3: true,
+// //       ),
+// //       home: const EmailLupaPasswordState(),
+// //     );
+// //   }
+// // }
 
-// class EmailLupaPassword extends StatefulWidget {
-//   const EmailLupaPassword({super.key});
+class EmailLupaPassword extends StatefulWidget {
+  const EmailLupaPassword({super.key});
 
-//   @override
-//   State<StatefulWidget> createState() {
-//     return EmailLupaPasswordState();
-//   }
-// }
+  @override
+  State<StatefulWidget> createState() {
+    return EmailLupaPasswordState();
+  }
+}
 
-// class EmailLupaPasswordState extends State<EmailLupaPassword> {
-//   TextEditingController email = TextEditingController();
+class EmailLupaPasswordState extends State<EmailLupaPassword> {
+  TextEditingController email = TextEditingController();
 
-//   @override
-//   void initState() {
-//     // TODO: implement initState
-//     super.initState();
-//   }
+  String emailErrorText = "";
+  void validateEmail(String value) {
+    if (value.isEmpty) {
+      setState(() {
+        emailErrorText = 'Email Dibutuhkan';
+      });
+    } else if (!isEmailValid(value)) {
+      setState(() {
+        emailErrorText = 'Email tidak Valid';
+      });
+    } else {
+      setState(() {
+        emailErrorText = "";
+      });
+    }
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           centerTitle: true,
-//           title: Text(
-//             'Lupa Password',
-//             style: TextStyle(
-//                 fontFamily: 'Poppins',
-//                 fontWeight: FontWeight.bold,
-//                 color: Colors.white),
-//             textAlign: TextAlign.center,
-//           ),
-//           backgroundColor: Theme.of(context).primaryColor,
-//         ),
-//         resizeToAvoidBottomInset: true,
-//         body: SingleChildScrollView(
-//           child: Column(
-//             children: [
-//               Padding(
-//                 padding: const EdgeInsets.only(
-//                     top: 10, bottom: 10, left: 10, right: 10),
-//                 child: Container(
-//                   child: Image(
-//                       width: MediaQuery.of(context).size.width,
-//                       image: AssetImage('assets/images/Logo.png'),
-//                       fit: BoxFit.fill),
-//                 ),
-//               ), //CONTAINER UNTUK LOGO
-//               const Padding(
-//                 padding: EdgeInsets.all(10),
-//                 child: Text(
-//                   "Lectio Divina",
-//                   style: TextStyle(
-//                       fontSize: 36,
-//                       fontWeight: FontWeight.bold,
-//                       fontFamily: "Poppins"),
-//                 ),
-//               ),
-//               Padding(
-//                 padding: const EdgeInsets.only(
-//                     top: 10, bottom: 0, left: 10, right: 10),
-//                 child: Container(
-//                   child: const Align(
-//                     alignment: Alignment.centerLeft,
-//                     child: Text(
-//                       "Sign Up",
-//                       style: TextStyle(
-//                         fontSize: 18,
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//               Padding(
-//                 padding: const EdgeInsets.all(10),
-//                 child: TextField(
-//                   onChanged: (value) {
-//                     setState(() {
-//                       error_register = "";
-//                     });
-//                     name = value;
-//                   },
-//                   decoration: const InputDecoration(
-//                       border: OutlineInputBorder(),
-//                       labelText: 'Nama',
-//                       hintText: 'Masukkan nama anda'),
-//                 ),
-//               ),
-//               Padding(
-//                 padding: const EdgeInsets.all(10),
-//                 child: TextField(
-//                   controller: email,
-//                   onChanged: (value) {
-//                     setState(() {
-//                       error_register = "";
-//                     });
-//                   },
-//                   decoration: const InputDecoration(
-//                       border: OutlineInputBorder(),
-//                       labelText: 'Email',
-//                       hintText: 'Masukkan Email anda'),
-//                 ),
-//               ),
-//               Padding(
-//                 padding: const EdgeInsets.all(10),
-//                 child: TextField(
-//                   controller: noHp,
-//                   onChanged: (value) {
-//                     setState(() {
-//                       error_register = "";
-//                     });
-//                   },
-//                   decoration: const InputDecoration(
-//                       border: OutlineInputBorder(),
-//                       labelText: 'No Handphone',
-//                       hintText: 'Masukkan no HP anda'),
-//                 ),
-//               ),
-//               Padding(
-//                 padding: const EdgeInsets.all(10),
-//                 child: TextField(
-//                   onChanged: (value) {
-//                     setState(() {
-//                       error_register = "";
-//                     });
-//                     username = value;
-//                   },
-//                   decoration: const InputDecoration(
-//                       border: OutlineInputBorder(),
-//                       labelText: 'username',
-//                       hintText: 'Masukkan username anda'),
-//                 ),
-//               ),
-//               Padding(
-//                 padding: const EdgeInsets.only(
-//                     top: 10, bottom: 0, left: 10, right: 10),
-//                 //padding: EdgeInsets.symmetric(horizontal: 15),
-//                 child: TextField(
-//                   obscureText: isObscuredRegister,
-//                   onChanged: (value) {
-//                     setState(() {
-//                       error_register = "";
-//                     });
-//                     password = value;
-//                   },
-//                   decoration: InputDecoration(
-//                       suffixIcon: IconButton(
-//                           padding: const EdgeInsetsDirectional.only(end: 12),
-//                           icon: isObscuredRegister
-//                               ? const Icon(Icons.visibility)
-//                               : const Icon(Icons.visibility_off),
-//                           onPressed: (() {
-//                             setState(() {
-//                               isObscuredRegister = !isObscuredRegister;
-//                             });
-//                           })),
-//                       border: const OutlineInputBorder(),
-//                       labelText: 'Kata Sandi',
-//                       hintText: 'Masukkan kata sandi anda'),
-//                 ),
-//               ),
-//               Padding(
-//                 padding: const EdgeInsets.only(
-//                     top: 10, bottom: 0, left: 10, right: 10),
-//                 //padding: EdgeInsets.symmetric(horizontal: 15),
-//                 child: TextField(
-//                   obscureText: isObscuredRepeatPassword,
-//                   onChanged: (value) {
-//                     setState(() {
-//                       error_register = "";
-//                     });
-//                     repeat_password = value;
-//                   },
-//                   decoration: InputDecoration(
-//                       suffixIcon: IconButton(
-//                           padding: const EdgeInsetsDirectional.only(end: 12),
-//                           icon: isObscuredRepeatPassword
-//                               ? const Icon(Icons.visibility)
-//                               : const Icon(Icons.visibility_off),
-//                           onPressed: (() {
-//                             setState(() {
-//                               isObscuredRepeatPassword =
-//                                   !isObscuredRepeatPassword;
-//                             });
-//                           })),
-//                       border: const OutlineInputBorder(),
-//                       labelText: 'Ulang Kata Sandi',
-//                       hintText: 'Masukkan ulang kata sandi anda'),
-//                 ),
-//               ),
-//               if (error_register != "")
-//                 Text(
-//                   error_register,
-//                   style: TextStyle(color: Colors.red),
-//                 ),
-//               Align(
-//                 alignment: Alignment.bottomCenter,
-//                 child: Container(
-//                   padding: EdgeInsets.only(top: 20),
-//                   child: Padding(
-//                     padding: const EdgeInsets.all(10),
-//                     child: GestureDetector(
-//                       onTap: () {
-//                         createAccount();
-//                       },
-//                       child: Container(
-//                         height: 40,
-//                         width: double.infinity,
-//                         decoration: BoxDecoration(
-//                             color: Theme.of(context).primaryColor,
-//                             borderRadius: BorderRadius.circular(5)),
-//                         child: const Center(
-//                           child: Text(
-//                             'Sign Up',
-//                             style: TextStyle(
-//                               color: Colors.white,
-//                               fontSize: 15,
-//                               fontWeight: FontWeight.bold,
-//                             ),
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//               Padding(
-//                 padding: const EdgeInsets.only(
-//                     top: 10, bottom: 20, left: 10, right: 10),
-//                 child: Row(
-//                   mainAxisAlignment: MainAxisAlignment.center,
-//                   children: [
-//                     const Text("Sudah memiliki akun ? "),
-//                     GestureDetector(
-//                       onTap: () {
-//                         Navigator.push(
-//                           context,
-//                           MaterialPageRoute(
-//                             builder: (context) => const MyLogin(),
-//                           ),
-//                         );
-//                       },
-//                       child: const Text(
-//                         "Login sekarang",
-//                         style: TextStyle(
-//                           fontWeight: FontWeight.bold,
-//                         ),
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               )
-//             ],
-//           ),
-//         ));
-//   }
-// }
+  bool isEmailValid(String email) {
+    // Basic email validation using regex
+    // You can implement more complex validation if needed
+    return RegExp(r'^[\w-\.]+@[a-zA-Z]+\.[a-zA-Z]{2,}$').hasMatch(email);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Lupa Password',
+            style: TextStyle(
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+        resizeToAvoidBottomInset: true,
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    "Lupa Password",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: email,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                        hintText: 'Masukkan Email Anda'),
+                    onChanged: validateEmail,
+                  ),
+                ),
+                Text(
+                  emailErrorText,
+                  style: TextStyle(color: Colors.red),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: InkWell(
+                    onTap: () {
+                      if (emailErrorText == "") {
+                        print("VALIDDDDDD");
+                      }
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Theme.of(context).primaryColor),
+                      child: const Center(
+                        child: Text(
+                          "Kirim Email",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
